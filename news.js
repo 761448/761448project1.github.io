3cac254218574c0fce3c0a2de02d6ebac89dedc7
761448 <46806198+761448@users.noreply.github.com>
2019-03-04 17:27:35 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,18 @@
+function news1(){
+    document.getElementById("news1").style.display="block";
+    document.getElementById("news2").style.display="none";
+    document.getElementById("news3").style.display="none";
+   
+
+}
+function news2(){
+    document.getElementById("news1").style.display="none";
+    document.getElementById("news2").style.display="block";
+    document.getElementById("news3").style.display="none";
+   
+}
+function news3(){
+    document.getElementById("news1").style.display="none";
+    document.getElementById("news2").style.display="none";
+    document.getElementById("news3").style.display="block";
+   
