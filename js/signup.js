966eb44aff09ef5dbb7d7df72352996bb7cf91c2
761448 <46806198+761448@users.noreply.github.com>
2019-03-08 17:23:18 +0530

    Add files via upload

--- js/signup.js
+++ js/signup.js
@@ -16,21 +16,27 @@ function validateForm(){
     }
     if(Email==""){
         alert("enter email address");
+        return false;
     }
     if(mobile==""){
         alert("enter mobile number");
+        return false;
     }
     if(pass1==""){
         alert("enter password");
+        return false;
     }
     if(pass2==""){
         alert("re-enter password");
+        return false;
     }
     if(pass1!=pass2){
-        alert("password didn't match")
+        alert("password didn't match");
+        return false;
     }
     if(pass1.length<8 || pass1.length>15){
         alert("password should be between 8 to 15 characters");
+        return false;
     }
    /**  else{
         if(pass1.matches([0-9]{1,},[a-zA-Z]{1,})) {}
@@ -40,4 +46,12 @@ function validateForm(){
 
         }*/
     
+    }
+
+    function signupfunction(){
+        var pass1=document.frm2.password1.value;
+        var pass2=document.frm2.password2.value;
+        var result=pass1.equals(pass2);
+        if(result){}
+        else{alert("password didn't match")}
     }
